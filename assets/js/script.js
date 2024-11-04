7a643f95bbea82491905f63fdea650e5312fabd2
Anne Billaut-Charavel <anne@macbook-pro-de-anne.local>
2024-11-04 20:24:46 +0100

    Added more quotes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,15 @@ const quotes = ["Have more than you show, speak less than you know.",
 
 "Was ever woman in this humour wooed? Was ever woman in this humour won?",
 
-"Love all, trust a few, do wrong to none."];
+"Love all, trust a few, do wrong to none.",
+
+"Shine out fair sun, till I have bought a glass, That I may see my shadow as I pass.",
+
+"The world is grown so bad that wrens make pray where eagles dare not perch",
+
+"I that have neither pity, love, nor fear.",
+
+"I have no brother, I am like no brother."];
 
 function getQuote() {
   var randomIndex = Math.floor(Math.random() * quotes.length);
