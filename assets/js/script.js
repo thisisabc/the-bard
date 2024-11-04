991c97d585a7b6185bf1ca510a70d2ff92578329
Anne Billaut-Charavel <anne@macbook-pro-de-anne.local>
2024-11-04 20:06:26 +0100

    Added a automatic switch betwen light and dark mode.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,4 @@ function getQuote() {
 }
 getQuote(); // on load
 
-function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
-}
-
-function toggleDarkMode() {
-  let isDark = document.body.classList.toggle('dark-mode');
-  localStorage.setItem('darkMode', isDark ? 'enabled' : 'disabled');
-}
-
-// On page load
-document.addEventListener('DOMContentLoaded', (event) => {
-  if (localStorage.getItem('darkMode') === 'enabled') {
-    document.body.classList.add('dark-mode');
-  }
-});
 
