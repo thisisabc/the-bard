94100e1e7b153fce8d39415ba876938a8bb63d14
Anne <anneblt@me.com>
2024-11-04 21:11:26 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,13 @@ const quotes = ["Have more than you show,\n speak less than you know.",
 
 "Love all, trust a few, do wrong to none.",
 
-"Shine out fair sun,\n till I have bought a glass,\n That I may see my shadow as I pass.",
+"Shine out fair sun,\rtill I have bought a glass,\n That I may see my shadow as I pass.",
 
 "The world is grown so bad that wrens make pray where eagles dare not perch",
 
 "I that have neither pity, love, nor fear.",
 
-"I have no brother,\n I am like no brother."];
+"I have no brother,\rI am like no brother."];
 
 function getQuote() {
   var randomIndex = Math.floor(Math.random() * quotes.length);
