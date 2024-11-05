09e3792433648f854cdf894b7928985521b1d235
Anne Billaut-Charavel <anne@macbook-pro-de-anne.local>
2024-11-05 16:14:17 +0100

    Minor changes :
    -removed quotes
    -cleaned up CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
-const quotes = ["Have more than you show,<br>speak less than you know.",
+const quotes = ["Have more than you show,<br>Speak less than you know.",
 
 "Harp not on that string.",
 
@@ -7,7 +7,7 @@ const quotes = ["Have more than you show,<br>speak less than you know.",
 
 "Love all, trust a few, do wrong to none.",
 
-"Shine out fair sun,<br>till I have bought a glass,<br>That I may see my shadow as I pass.",
+"Shine out fair sun,<br>Till I have bought a glass,<br>That I may see my shadow as I pass.",
 
 "The world is grown so bad that wrens make pray where eagles dare not perch",
 
